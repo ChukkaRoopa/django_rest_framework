cb89978e7dcbcd6d7a60346bd405bc12c62c41b1
Roopa Chukka <roopachukka@Roopas-MacBook-Air.local>
2025-04-21 14:56:46 +0530

    class based views

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     # get a single student
     path('students/<int:pk>/', views.sudentDetailView),
+
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
 ]
