3cac7a05854047e26da752e7284c0e2029ac5caa
Roopa Chukka <roopachukka@Roopas-MacBook-Air.local>
2025-04-24 23:48:48 +0530

    implemented nested serializers, pagination(blogs and comments)

--- api/urls.py
+++ api/urls.py
@@ -16,5 +16,11 @@ urlpatterns = [
     # path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
 
     # using viewset
-    path('', include(router.urls))
+    path('', include(router.urls)),
+
+    path('blogs/', views.BlogsView.as_view()),
+    path('comments/', views.CommentsView.as_view()),
+
+    path('blogs/<int:pk>/', views.BlogDetailView.as_view()),
+    path('comments/<int:pk>/', views.CommentDetailView.as_view()),
 ]
